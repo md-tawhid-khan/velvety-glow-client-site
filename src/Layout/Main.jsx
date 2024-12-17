0991540264897b961fe6a2df7a76a78876cd6558
md1300 <md.tawhid.khan1998@gmail.com>
2024-12-18 05:01:53 +0600

    create navbar design

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Share/Navbar/Navbar";
 
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet/>
             
         </div>
---
+++ src/Share/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import { BsSearchHeart } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+const Navbar = () => {
+  return (
+    <>
+      <div className="hidden md:flex justify-around">
+      <div className="text-3xl font-extrabold font-serif bg-gradient-to-r from-pink-500 to-pink-900 text-transparent bg-clip-text">Velvety Glow</div>
+        
+        <div className="join">
+          <div>
+            <div>
+              <input
+                className="input input-bordered join-item"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+          <select className="select select-bordered join-item">
+            <option disabled selected>
+              Filter
+            </option>
+            <option>Sci-fi</option>
+            <option>Drama</option>
+            <option>Action</option>
+          </select>
+          <div>
+            <button className="btn join-item bg-pink-500">
+              <BsSearchHeart className="text-3xl" />
+            </button>
+          </div>
+        </div>
+
+        <div>
+            <div className="flex items-center gap-4">
+                <FaRegUserCircle className="text-3xl"/>
+            <h1 className="text-3xl  bg-gradient-to-r from-pink-500 to-pink-900 text-transparent bg-clip-text "> <span>Sign in </span> / <span>Sign Up</span> </h1>
+            </div>
+        </div>
+
+       <div className="flex gap-4 items-center">
+       <div className="indicator">
+            <h1><IoCartOutline className="text-3xl" /></h1>
+            <p className="badge badge-sm indicator-item">0</p>
+            
+        </div>
+        <div>
+            <h1 className="text-3xl"> My blog ( <span>0</span> )</h1>
+        </div>
+       </div>
+     
+      </div>
+
+      
+    </>
+  );
+};
+
+export default Navbar;
